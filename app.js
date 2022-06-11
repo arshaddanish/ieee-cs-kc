e990e866a8ee24c3b7878c44e105f1f08bf5b122
Arshad Danish <arshaddanishthana@gmail.com>
2022-06-11 12:18:38 +0530

    udate

---
+++ app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running at port 3000");
+});
