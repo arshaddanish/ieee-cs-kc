a9ce8d78dbbe7ba62f9d4a5dc5a54ff7c09331e4
Rishan KP <rishangiggs11@gmail.com>
2022-09-26 13:56:58 +0530

    add events,updates,blogs

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+  db: {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB_NAME || 'mydb',
+  },
+  listPerPage: 10,
+};
+
+module.exports = config;
